9c8bc54d28751d5ffac7e28661a07b6e3d8f2d29
alexlee <102556710+alexlee0422@users.noreply.github.com>
2025-11-13 22:20:56 -0600

    Update combined_streamlit_companies.py

--- combined_streamlit_companies.py
+++ combined_streamlit_companies.py
@@ -18,7 +18,7 @@ from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
 from langchain_core.runnables import RunnableLambda
 from langchain_core.prompts import ChatPromptTemplate
 from streamlit_gsheets import GSheetsConnection
-
+import gspread
 
 # --- CONFIGURATION ---
 LOCAL_DATA_FILE = "data/merged_data.csv"
